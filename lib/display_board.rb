38c7c8595a7955eb59bd4c6cd6f65cf3ada5e19e
Christopher Del Basso <ccdelbasso@gmail.com>
2021-03-22 19:48:28 -0500

    commit tree c72dc34a5aa0f000dd17c7d139cc9a915fdb806a

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,25 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+empty_board = ["   ","   ","   ","   ","   ","   ","   ","   ","  "]
+center_X_board = ["   ","   ","   ","   "," X  ","   ","   ","   ","  "]
+top_left_O_board = [" O ","   ","   ","   ","   ","   ","   ","   ","  "]
+top_left_O_center_X_board = [" O ","   ","   ","   "," X ","   ","   ","   ","  "]
+winning_X_via_top_row = [" X "," X "," X ","   ","   ","   ","   ","   ","  "]
+winning_O_via_bottom_row = ["   ","   ","   ","   ","   ","   "," O "," O "," O "]
+winning_X_top_left_to_bottom_right_diagnol = [" X ","   ","   "," X ","   ","   ","   ","   "," X "]
+winning_O_top_right_to_bottom_left_diagnol = ["   ","   "," O ","   "," O ","   "," O ","   ","   "]
+random_board = ["   ","   ","   "," O ","   ","   ","   "," X ","  "]
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+display_board(empty_board)
+display_board(center_X_board)
+display_board(top_left_O_board)
+display_board(top_left_O_center_X_board)
+display_board(winning_X_via_top_row)
+display_board(winning_O_via_bottom_row)
+display_board(random_board)
